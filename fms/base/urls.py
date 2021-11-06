e693a62711cfd1445dddc98fd9fc7d7183290302
Aleksandra-buna <borabora0604@gmail.com>
2021-11-06 17:50:53 +0300

    creating a "Register" button

--- fms/base/urls.py
+++ fms/base/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login_register, name='login'),
+    path('login/', views.login_page, name='login'),
     path('logout/', views.logout_event, name='logout'),
+    path('register/', views.register_page, name='register'),
     path('room/<str:pk>/', views.room_page, name='room'),
     path('create-room/', views.create_room, name='create-room'),
     path('update-room/<str:pk>/', views.update_room, name='update-room'),
--- fms/base/views.py
+++ fms/base/views.py
@@ -1,15 +1,17 @@
 from django.db.models import Q
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Room, Topic
 from .forms import RoomForm
 
 
-def login_register(request):
+def login_page(request):
+    page = 'login'
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         user = authenticate(request, username=username, password=password)
@@ -20,7 +22,7 @@ def login_register(request):
         else:
             messages.error(request, 'Пользователь или пароль введены не верно!')
 
-    context = {}
+    context = {'page': page}
 
     return render(request, 'base/login.html', context)
 
@@ -30,10 +32,29 @@ def logout_event(request):
     return redirect('home')
 
 
+def register_page(request):
+    form = UserCreationForm
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Пароли не совпадают, попробуйте снова')
+
+    context = {'form': form}
+
+    return render(request, 'base/login.html', context)
+
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') is not None else ''
     topics = Topic.objects.all()
-    rooms = Room.objects.filter(Q(name__icontains=q))
+    rooms = Room.objects.filter(Q(name__icontains=q) |
+                                Q(topic__name__icontains=q))
 
     count_rooms = rooms.count()
 
